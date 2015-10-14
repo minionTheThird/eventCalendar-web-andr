742af70dca8e1bca9bee56bc2f378e5d526fb079
minionTheThird <sanjaysingh.mt3@gmail.com>
2015-10-14 21:56:42 +0530

    Added hackerEarth Calendar

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(
     function()
     {
         getCodeforcesEvents();
+        getHackerEarthEvents();
     }
 );
 
@@ -42,7 +43,7 @@ function getCodeforcesEvents()
                 for(var id in events){
                   if(events[id].phase != "FINISHED"){
 
-                    var row = table.insertRow(0);
+                    var row = table.insertRow(1);
                     var l_calendar = row.insertCell(0);
 
                     var start_date = new Date(events[id].startTimeSeconds*1000);
@@ -50,7 +51,7 @@ function getCodeforcesEvents()
                     start_date.format();
                     end_date.format();
 
-                    var template = $('#codeforces-template').html();
+                    var template = $('#calendar-template').html();
                     Mustache.parse(template);   // optional, speeds up future uses
                     var rendered = Mustache.render(
                                       template,
@@ -58,6 +59,69 @@ function getCodeforcesEvents()
                                         month : start_date.MMM,
                                         day : start_date.DD,
                                         name : events[id].name,
+                                        startTime : start_date.TIME,
+                                        endTime : end_date.TIME,
+                                        timeZone : start_date.timeZone,
+                                        startDateTime : start_date.dateTime ,
+                                        endDateTime : end_date.dateTime
+                                      });
+                    l_calendar.innerHTML = rendered;
+
+                  }
+                 };
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log("error for : " + this.handle)
+            }
+
+        });
+}
+
+function getHackerEarthEvents()
+{
+        console.log("inside HE");
+        var inputHTML = "";
+        var calendarId = 'hackerearth.com_73f0o8kl62rb5v1htv19p607e4@group.calendar.google.com'
+        var startTime = new Date();
+        var endTime = new Date();
+        var key = 'AIzaSyCAkVQVwMzmPHxbaLUAqvb6dYUwjKU5qnM';
+        //usage limit to http://minionthethird.github.io/*
+        //working = AIzaSyCAkVQVwMzmPHxbaLUAqvb6dYUwjKU5qnM
+        //other key = AIzaSyBQal2rNhP5SRkU5hZytY7Yb8nYc5Q1nrc
+        endTime.setDate(endTime.getDate()+7);
+        var URL = 'https://www.googleapis.com/calendar/v3/calendars/'+calendarId+'/events?key='+key+'&timeMax='+endTime.toISOString()+'&timeMin='+startTime.toISOString();
+        $.ajax({
+            url: URL,
+            dataType: 'JSONP',
+            type: 'GET',
+            success: function (dataHE) {
+              console.log("inside he success")
+                var events = dataHE.items;
+                events.sort(function(a, b) {
+                    return b.start.dateTime.localeCompare(a.start.dateTime);
+                });
+
+                var table = document.getElementById("hackerEarth");
+
+                for(var id in events){
+                  if(events[id].status == "confirmed"){
+
+                    var start_date = new Date(events[id].start.dateTime);
+                    var end_date = new Date(events[id].end.dateTime);
+                    start_date.format();
+                    end_date.format();
+
+                    var row = table.insertRow(1);
+                    var l_calendar = row.insertCell(0);
+
+                    var template = $('#calendar-template').html();
+                    Mustache.parse(template);   // optional, speeds up future uses
+                    var rendered = Mustache.render(
+                                      template,
+                                      {
+                                        month : start_date.MMM,
+                                        day : start_date.DD,
+                                        name : events[id].summary,
                                         startTime : start_date.TIME,
                                         endTime : end_date.TIME,
                                         timeZone : start_date.timeZone,
